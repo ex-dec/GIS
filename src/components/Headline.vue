208a3c7406f4d86d337bf19e0640d7aacd71ac87
MRizqy2 <mrfrizqy22@gmail.com>
2023-05-31 14:09:08 +0700

    update landing page

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -5,9 +5,10 @@
 		<img src="/vector.png" class="md:w-5/12 block mb-8 lg:mb-0" />
 		
 		<!-- Caption -->
-		<section class="md:w-6/12">
+		<section class="md:w-6/11">
 			<h1 id="heading" class="lg:text-5xl lg:leading-snug text-3xl text-gray-50 font-semibold">
-				Pemetaan Rawan Longsor Daerah Gunung Penanggungan
+				Pemetaan Rawan 
+				<br />Longsor Daerah Gunung Penanggungan
 			</h1>
 			<p class="lg:text-2xl text-gray-100 font-medium mt-4">Keselamatan :</p>
 			<p class="lg:text-2xl text-gray-100 font-medium">
@@ -15,7 +16,7 @@
 			</p>
 			
 			<!-- Call to action -->
-			<a href="" class="duration-300 cursor-pointer active:ring-2 ring-indigo-300 hover:scale-90 text-gray-50 w-8/12 mt-4 lg:mt-8 justify-center flex items-center gap-2 lg:gap-3 bg-gradient-to-r from-indigo-500 to-indigo-700 py-3 px-3 lg:py-4 rounded-lg block">
+			<a href="https://www.google.com/maps/place/Gn.+Penanggungan/@-7.6166019,112.6154816,15z/data=!4m6!3m5!1s0x2dd7d8a014c487ad:0xc3bad5c8087f05f2!8m2!3d-7.6150282!4d112.6201594!16s%2Fm%2F027bb2r!5m1!1e4?entry=ttu" class="duration-300 cursor-pointer active:ring-2 ring-indigo-300 hover:scale-90 text-gray-50 w-8/12 mt-4 lg:mt-8 justify-center flex items-center gap-2 lg:gap-3 bg-gradient-to-r from-indigo-500 to-indigo-700 py-3 px-3 lg:py-4 rounded-lg block">
 				<i class="fa fa-map lg:text-xl"></i>
 				<span class="lg:text-xl">Open Map</span>
 			</a>
